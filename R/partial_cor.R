889bd82a3f844111c6cd6726d9047a31b66ff93c
kg737 <47535841+kg737@users.noreply.github.com>
2019-03-07 15:19:19 -0500

    Update partial_cor.R

--- R/partial_cor.R
+++ R/partial_cor.R
@@ -1,21 +1,22 @@
-#' @title Data preprocessing for partial correlaton analysis
+#' @title Partial correlaton analysis
 #' @description A method that integrates differential expression (DE) analysis
 #'   and differential network (DN) analysis to select biomarker candidates for
 #'   survival time prediction. partial_cor is the second step of partial correlation
 #'   calculation after the output result from select_rho_partial function
-#' @param data_list list of pre-processed data from select_rho_partial function
-#' @param rho_group1 rule to choose rho for group 1, "min": minimum rho,
-#' "ste" one standard error from minimum, or user can input rho of their choice, default: minimum
-#' @param rho_group2 rule to choose rho for group 1, "min": minimum rho,
-#' "ste" one standard error from minimum, or user can input rho of their choice, default: minimum
-#' @param p_val optional, a dataframe contains p values for each metabolite/molecule
-#' @param permutation a positive integer of desired number of permutations, default 1000
-#' @param permutation_thres threshold for permutation, defalut is 0.025 for each side to make 95percent
+#' @param data_list This is a list of pre-processed data outputed by the select_rho_partial function
+#' @param rho_group1 This is the rule for choosing rho for group 1, "min": minimum rho,
+#' "ste" one standard error from minimum, or user can input rho of their choice, the default is minimum
+#' @param rho_group2 This is the rule for choosing rho for group 1, "min": minimum rho,
+#' "ste" one standard error from minimum, or user can input rho of their choice, the default is minimum
+#' @param p_val This is optional.It is a dataframe that contains p-values for each metabolite/molecule
+#' @param permutation This is a positive integer of the desired number of permutations. The default is 1000 permutations
+#' @param permutation_thres This is the threshold for permutation. The defalut is 0.025 for each side to make 95percent
 #' @examples preprocess<- select_rho_partial(data=Met_GU,class_label =
 #'    Met_Group_GU,id=Met_name_GU,error_curve="YES")
 #'    partial_cor(data_list=preprocess,rho_group1='min',
 #'    rho_group2="min",permutation = 1000,p_val=pvalue_M_GU,permutation_thres=0.05)
-#' @return a list containing a score dataframe and a differential network dataframe
+#' @return A list containing a score dataframe with "MetID", "P_value", "Node Degree", "Activity_Score"
+#'          and a list containing a differential network dataframe with  "Node1", "Node2", the binary link value and the weight link value
 #' @import devtools
 #' @importFrom glasso glasso
 #' @importFrom stats qnorm cor quantile var sd glm
@@ -127,5 +128,6 @@ partial_cor <- function(data_list =NULL, rho_group1=NULL,rho_group2=NULL, permut
         indeed_df<-indeed_df[order(indeed_df$Activity_Score, decreasing=TRUE), ]
 
         result_list <-list(activity_score=indeed_df,diff_network=edge_dn)
+        return(result_list)
     }
 }
