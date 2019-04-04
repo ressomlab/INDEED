f4fd521e6c2ebfaa479a1e9a8b2a4b7a2fa0d26b
Ressom Omics Lab <38534764+ressomlab@users.noreply.github.com>
2019-04-04 12:52:02 -0700

    Update documentation

--- R/INDEED.R
+++ R/INDEED.R
@@ -1,7 +1,7 @@
 #' INDEED: A package for biomarker candidate prioritization.
 #'
-#' The INDEED package provides a important functions below:
-#' select_rho_partial
+#' The INDEED package provides an important functions below:
+#' select_rho_partial, partial_cor and non_partial_cor.
 #'
 #' @section select_rho_partial function:
 #' select_rho_partial function preprocess data for partical correlation analysis,
@@ -10,16 +10,16 @@
 
 #' @section partial_cor function:
 #' partial_cor function performs partical correlation analysis
-#' user input preprocessed list from select_rho_partial step
+#' based on user input preprocessed list from select_rho_partial step
 #' and the rho choosing method or rho of their choice
-#' and number of permutations (default 1000), p-value is optional
+#' and number of permutations (default 1000), p-value is optional,
 #' the result of score table and differential network will be returned
 
 #' @section non_partial_cor function:
 #' non_partial_cor function performs correlation analysis
-#' user input data,class label,p-value, sample id,
-#' number of permutations, and method(default pearson)
-#' p value is optional
+#' based on user input data, class label, p-value, sample id,
+#' number of permutations, and method (default pearson)
+#' p value is optional,
 #' the result of score table and differential network will be returned
 #'
 #' @docType package
